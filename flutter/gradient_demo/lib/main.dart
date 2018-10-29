17db24ed1c7276fa20b13bd0bf6793e13b7ee383
Csaba Szugyiczki <szugyiczkicsaba@gmail.com>
2018-10-29 23:33:52 +0100

    Add projects

---
+++ flutter/gradient_demo/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [const Color(0xFF00BCD4), const Color(0xFF009688), const Color(0xFF4CAF50)],
+          ),
+        ),
+      ),
+    );
+  }
+}
